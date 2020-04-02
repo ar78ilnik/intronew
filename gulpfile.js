89c9b81ee36e5514cd687f3c45536dce9faa903c
ar78ilnik <arakchiev.ilya@yandex.ru>
2020-04-02 21:12:53 +0300

    Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -17,71 +17,71 @@ const autoprefixer = require('autoprefixer');
 const del = require('del');
 
 function clean() {
-    return del('dist');
+  return del('dist');
 };
 
 function copy() {
-    return gulp.src([
+  return gulp.src([
         'app/fonts/**/*.*',
         'app/img/**/*.{jpg,png,webp}',
         'app/js/*.*'
     ], {
-            base: 'app',
-        }).pipe(gulp.dest('dist'))
-        .pipe(debug({
-            title: 'copy'
-        }));
+      base: 'app',
+    }).pipe(gulp.dest('dist'))
+    .pipe(debug({
+      title: 'copy'
+    }));
 };
 
 function style() {
-    return gulp.src('app/scss/style.scss')
-        .pipe(plumber())
-        .pipe(debug({
-            title: 'src'
-        }))
-        .pipe(scss())
-        .pipe(debug({
-            title: 'scss'
-        }))
-        .pipe(postcss([
+  return gulp.src('app/scss/style.scss')
+    .pipe(plumber())
+    .pipe(debug({
+      title: 'src'
+    }))
+    .pipe(scss())
+    .pipe(debug({
+      title: 'scss'
+    }))
+    .pipe(postcss([
         autoprefixer()
     ]))
-        .pipe(gulp.dest('dist/css'))
-        .pipe(csso())
-        .pipe(rename('style.min.css'))
-        .pipe(debug({
-            title: 'rename'
-        }))
-        .pipe(gulp.dest('dist/css'))
-        .pipe(bs.reload({
-            stream: true
-        }))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(csso())
+    .pipe(rename('style.min.css'))
+    .pipe(debug({
+      title: 'rename'
+    }))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(bs.reload({
+      stream: true
+    }))
 };
 
 function html() {
-    return gulp.src('app/pages/**/*.html', {
-            since: gulp.lastRun('html')
-        })
-        .pipe(debug({
-            title: 'html'
-        }))
-        .pipe(gulp.dest('dist'))
+  return gulp.src('app/pages/**/*.html', {
+      since: gulp.lastRun('html')
+    })
+    .pipe(debug({
+      title: 'html'
+    }))
+    .pipe(gulp.dest('dist'))
 };
 
 function watch() {
-    gulp.watch('app/pages/**/*.html')
-    gulp.watch('app/scss/*.scss', style);
-    gulp.watch('app/js/**/*.js', copy);
-    gulp.watch('app/fonts/*.{woff, woff2}');
+  gulp.watch('app/pages/**/*.html', html)
+  gulp.watch('app/scss/*.scss', style);
+  gulp.watch('app/js/**/*.js', copy);
+  gulp.watch('app/fonts/*.{woff, woff2}');
 };
 
 function server() {
-    bs.init({
-        server: 'dist'
-    });
-    bs.watch('app/pages/**/*.html').on('change', bs.reload);
-    bs.watch('app/scss/*.scss').on('change', bs.reload);
-    bs.watch('app/js/**/*.js').on('change', bs.reload);
+  bs.init({
+    server: 'dist'
+  });
+  bs.watch('app/pages/**/*.html').on('change', bs.reload);
+  bs.watch('app/scss/*.scss').on('change', bs.reload);
+  bs.watch('app/js/**/*.js').on('change', bs.reload);
 };
 
 const build = gulp.series(clean, copy, style, html, gulp.parallel(watch, server));
