a38bfd89f959f56736b5cb9b9202ebf7bb0cd5de
ar78ilnik <arakchiev.ilya@yandex.ru>
2020-04-09 20:51:47 +0300

    Footer

--- app/js/menu-services.js
+++ app/js/menu-services.js
@@ -1,11 +1,13 @@
 'use strict';
 
+// Открытие меню "Наши услуги"
 var linkHead = document.querySelector('.services__head');
 var menu = document.querySelector('.services__menu');
 var items = menu.querySelectorAll('.services__submenu-item');
 
 linkHead.addEventListener('click', function () {
-    console.log(menu);
-    menu.classList.toggle('services__menu_opened');
+  console.log(menu);
+  menu.classList.toggle('services__menu_opened');
 });
 
+// Открытие меню в footer
