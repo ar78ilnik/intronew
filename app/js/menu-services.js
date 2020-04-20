949aa7146899ab96c46236275d97684d8e275e9b
ar78ilnik <arakchiev.ilya@yandex.ru>
2020-04-21 00:39:45 +0300

    Menu

--- app/js/menu-services.js
+++ app/js/menu-services.js
@@ -6,7 +6,6 @@ var menu = document.querySelector('.services__menu');
 var items = menu.querySelectorAll('.services__submenu-item');
 
 linkHead.addEventListener('click', function () {
-  console.log(menu);
   menu.classList.toggle('services__menu_opened');
 });
 
--- app/js/setup.js
+++ app/js/setup.js
@@ -5,12 +5,12 @@ input.setAttribute('checked', true);
 
 var popup = document.querySelector('.popup');
 
-var burger = document.querySelector('.main-nav__item_burger');
+var burger = document.querySelector('.nav__item_burger');
 burger.addEventListener('click', function () {
-    popup.classList.add('popup_opened');
+  popup.classList.add('popup_opened');
 });
 
 var closePopup = document.querySelector('.logo__close');
 closePopup.addEventListener('click', function () {
-    popup.classList.remove('popup_opened');
+  popup.classList.remove('popup_opened');
 });
