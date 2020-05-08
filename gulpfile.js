984f976cdea6b24cc1d4af434c3d3b6620a9d503
ar78ilnik <arakchiev.ilya@yandex.ru>
2020-05-08 17:46:17 +0300

    Slider with text

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ function clean() {
 function copy() {
   return gulp.src([
         'app/fonts/**/*.*',
-        'app/img/**/*.{jpg,png,webp}',
+        'app/img/**/*.{jpg,png,webp,mp4}',
         'app/js/*.*'
     ], {
       base: 'app',
